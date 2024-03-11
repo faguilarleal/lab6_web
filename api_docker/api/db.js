e1f0d4ded7911dfd6ddc2b1033bc2f18159cb1a6
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2024-03-10 21:08:12 -0600

    lint and endpoints

--- api_docker/api/db.js
+++ api_docker/api/db.js
@@ -1,35 +1,39 @@
 import conn from './connection.js'
 
 export async function getAllBlogs() {
-    try {
-        const [rows] = await conn.query('SELECT * FROM blogs')
-        return rows
-
-    } catch (e) {
-        console.log(e)
-        return e
-    }
+  try {
+    const [rows] = await conn.query('SELECT * FROM blogs')
+    return rows
+  } catch (e) {
+    console.log(e)
+    return e
+  }
+}
+export async function update(title, content) {
+  try {
+    const [result] = await conn.query(`UPDATE blogs SET content = '${content}' WHERE title = '${title}'`)
+    return result
+  } catch (e) {
+    return e
+  }
 }
 
 export async function createBlog(title, content) {
-    try {
-        const [result] = await conn.query(`INSERT INTO blogs (title, content) VALUES ('${title}', '${content}')`)
-        return result
-
-    } catch (e) {
-        console.log(e)
-        return e
-    }
+  try {
+    const [result] = await conn.query(`INSERT INTO blogs (title, content) VALUES ('${title}', '${content}')`)
+    return result
+  } catch (e) {
+    console.log(e)
+    return e
+  }
 }
 
-
 export async function deleteBlog(id) {
-    try {
-        const [result] = await conn.query(`DELETE FROM blogs WHERE id = ${id}`)
-        return result
-
-    } catch (e) {
-        console.log(e)
-        return e
-    }
+  try {
+    const [result] = await conn.query(`DELETE FROM blogs WHERE id = ${id}`)
+    return result
+  } catch (e) {
+    console.log(e)
+    return e
+  }
 }
--- api_docker/api/index.js
+++ api_docker/api/index.js
@@ -1,18 +1,13 @@
 import express from 'express'
 // import cors from 'cors'
-import { getAllBlogs, createBlog, deleteBlog } from './db.js'
+import {
+  getAllBlogs, createBlog, deleteBlog, update,
+} from './db.js'
+
 const app = express()
 const port = 3000
 // This line is necessary to parse the request body
 app.use(express.json())
-
-
-
-
-
-
-
-
 
 // console.log('enable Cors');
 // app.use(cors())
@@ -26,21 +21,47 @@ app.listen(port, () => {
 })
 
 app.get('/blogs', async (req, res) => {
-  const blogs = await getAllBlogs()
-  res.json(blogs)
+  try {
+    const blogs = await getAllBlogs()
+    res.json(blogs)
+  } catch (error) {
+    res.status(500).send('Error interno del servidor')
+  }
 })
 
+// eslint-disable-next-line consistent-return
 app.post('/blogs', async (req, res) => {
   const [title, content] = [req.body.title, req.body.content]
-  console.log(title, content);
+  if (!title || !content) {
+    return res.status(400).send('Falta título o contenido en la solicitud')
+  }
+  console.log(title, content)
   const blogs = await createBlog(title, content)
   res.json(blogs)
 })
 
-app.delete('/blogs/:id', async(request, response)=>{
-  console.log('delete blog');
-  const id = request.params.id
-  console.log(id);
+// eslint-disable-next-line consistent-return
+app.put('/blogs/:id', async (req, res) => {
+  try {
+    const [title, content] = [req.body.title, req.body.content]
+    if (!title || !content) {
+      return res.status(400).send('Falta título o contenido en la solicitud')
+    }
+    update(title, content)
+  } catch (error) {
+    res.status(500).json({ message: 'error updating the character' })
+  }
+})
+
+app.delete('/blogs/:id', async (request, response) => {
+  console.log('delete blog')
+  const { id } = request.params
+  console.log(id)
   const result = await deleteBlog(id)
   response.json(result)
+})
+
+// Middleware para manejar métodos HTTP no implementados
+app.use((req, res) => {
+  res.status(501).send('Método HTTP no implementado')
 })
