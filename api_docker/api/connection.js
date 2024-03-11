73cdf4e6fbc292b0ea062fd5bff321b1e4138c1b
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2024-03-10 18:12:27 -0600

    initial commit

---
+++ api_docker/api/connection.js
@@ -0,0 +1,17 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    // host:'mysql',
+    port: 33068,
+    user: 'blog_user',
+    database: 'blog_db',
+    password: 'blog_password',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+export default pool
+
+// conexion para el sql, es el conector 
---
+++ mysql_docker/schema.sql
@@ -0,0 +1,15 @@
+-- crear todo lo de la base de datos antes de iniciarla 
+
+CREATE DATABASE IF NOT EXISTS `blog_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `blog_db`;
+
+-- Create a user and grant privileges
+CREATE USER IF NOT EXISTS 'blog_user'@'%' IDENTIFIED BY 'blog_password';
+GRANT ALL PRIVILEGES ON blog_db.* TO 'blog_user'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS blogs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
+);
